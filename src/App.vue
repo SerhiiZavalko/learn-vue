b659762b52114633e90ac3da6520c9680916f36e
SerhiiZavalko <sergey.zavalko@gmail.com>
2020-02-24 06:16:38 +0200

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a href="#" class="navbar-brand">Learn Vue: ToDo</a>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="card" v-for="(item, index) in todos" :key="index">
+        <div class="card-body">
+          {{item.title}}
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      todos: [
+        {title: 'Setup environment'},
+        {title: 'Create main list of components'},
+        {title: 'Display TODO items'}
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.card {
+  margin-top: 15px;
 }
+
 </style>
